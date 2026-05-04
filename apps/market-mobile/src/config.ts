7f1c22e3674fe0bab8bd3ce35980580c4df7cdfe
bruce <bruce@betterbit.tech>
2026-05-04 16:16:31 +0800

    Point mobile app to server IP

--- apps/market-mobile/src/config.ts
+++ apps/market-mobile/src/config.ts
@@ -1,3 +1,3 @@
-export const WEB_BASE_URL = "http://192.168.31.87:8000";
-export const API_BASE_URL = "http://192.168.31.87:8000";
+export const WEB_BASE_URL = "http://150.109.22.77:8000";
+export const API_BASE_URL = "http://150.109.22.77:8000";
 export const FOREGROUND_POLL_MS = 15000;
